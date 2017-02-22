e8b528d0e4ce5edd3158fa639d6e175321eaf449
goose <jaranez@jointheindustry.com>
2017-02-22 14:04:33 -0800

    Added test file -  Selenium project

--- test.py
+++ test.py
@@ -1,18 +1,51 @@
-from BeautifulSoup import BeautifulSoup
-import re
-import urllib2
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = {'performance': 'ALL'}
 
+driver = webdriver.Chrome('./driver/chromedriver')
+#driver.set_window_size(375,667)
+driver.get('https://industry-fe-dev.herokuapp.com')
+login = driver.find_element_by_name('email')
+login.send_keys('josephtrop@gmail.com')
+login = driver.find_element_by_name('password')
+login.send_keys('P)o9I*u7')
+login.submit()
+#//*[@id="main"]/section/section/header/section/div[3]/span
 
-url = 'https://losangeles.craigslist.org/sgv/fbh/6006913779.html';
-soup = BeautifulSoup(urllib2.urlopen(url).read());
-reply = soup.findAll('a', attrs = {"id": "replylink"});
-print (reply)
-if reply:
-   url = 'http://losangeles.craigslist.org' + reply[0].get('href');
-   soup = BeautifulSoup(urllib2.urlopen(url).read());
-   reply = soup.findAll(['div', 'a','p'], attrs = {"class": re.compile(r'anonemail|mailapp')});
-   print( reply)
-   if reply:
-      reply[0].getText()
+time.sleep(5)
+driver.find_element_by_css_selector('.hamburger').click()
+time.sleep(2)
+driver.find_element_by_css_selector('.fa-user').click()
+time.sleep(2)
+driver.find_element_by_css_selector('.fa-pencil').click()
+time.sleep(2)
+username =driver.find_element_by_name('username')
+username.clear()
+time.sleep(2)
+username.send_keys('wawawawa')
+driver.get('https://industry-fe-dev.herokuapp.com')
+time.sleep(10)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+
+driver.save_screenshot('test.png')
+
+print (driver.find_element_by_css_selector('.shout-ca112rd'))
+
+xpath='//*[@id="left-content"]/div[1]/div/div[3]/form/div[3]/div[2]/span'
+driver.find_element_by_xpath(xpath).click()
+
+performance_log = driver.get_log('performance')
+print (str(performance_log).strip('[]'))
+print(driver.current_url)
+for entry in driver.get_log('performance'):
+   print (entry)
+driver.quit()
